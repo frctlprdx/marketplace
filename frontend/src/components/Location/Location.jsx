47fe09db86587435ac7cedb04c998c34783521c8
frctlprdx <ifam4@yahoo.com>
2025-08-18 08:28:13 +0700

    Fix

--- frontend/src/components/Location/Location.jsx
+++ frontend/src/components/Location/Location.jsx
@@ -1,41 +1,50 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 // Fix icon default supaya marker muncul
-import iconUrl from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 let DefaultIcon = L.icon({
   iconUrl,
   shadowUrl: iconShadow,
   iconSize: [25, 41],
-  iconAnchor: [12, 41]
+  iconAnchor: [12, 41],
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const Location = () => {
-  const center = [-7.250445, 112.768845]; // Ganti dengan koordinat lokasi desamu
+  const start = [-7.3613965, 110.4262988]; // Nogosaren
+  const destination = [-7.3615489, 110.4263604]; // JCQG+9GQ Nagasaren
+  const center = destination; // fokus ke tujuan
 
   return (
     <div className="w-screen flex flex-col items-center py-10 max-w-7xl px-4 sm:px-0 mx-auto h-screen">
       <div className="flex items-center w-full sm:mb-6 ">
-        <h1 className="text-2xl sm:text-3xl font-bold">Cari tahu lokasi kami</h1>
+        <h1 className="text-2xl sm:text-3xl font-bold">
+          Cari tahu lokasi kami
+        </h1>
       </div>
 
       <div className="flex items-center w-full h-screen z-0">
         <MapContainer
           center={center}
-          zoom={6}
+          zoom={6``}
           scrollWheelZoom={true}
-          style={{ height: '100%', width: '100%' }}
+          style={{ height: "100%", width: "100%" }}
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={center}>
-            <Popup>Desa Nogosaren</Popup>
+
+          <Marker position={start}>
+            <Popup>Desa Nogosaren (Start)</Popup>
+          </Marker>
+
+          <Marker position={destination}>
+            <Popup>JCQG+9GQ Nagasaren (Tujuan)</Popup>
           </Marker>
         </MapContainer>
       </div>
